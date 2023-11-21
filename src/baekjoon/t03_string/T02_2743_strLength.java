370fc1875fc0aaa3fa5a5044387578975cfb2d8a
mmminggu <mnyu1017@gmail.com>
2023-11-21 21:21:27 +0900

    백준 - 문자열

---
+++ src/baekjoon/t03_string/T02_2743_strLength.java
@@ -0,0 +1,13 @@
+package baekjoon.t03_string;
+
+import java.util.Scanner;
+// 2743번 단어 길이 재기
+// pulljima         8
+public class T02_2743_strLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+        sc.close();
+    }
+}
