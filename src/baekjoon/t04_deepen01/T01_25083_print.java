2942617b3ef6cd35b67fac9760a98affb3a968d0
mmminggu <mnyu1017@gmail.com>
2023-12-13 20:43:28 +0900

    백준 - 심화1

---
+++ src/baekjoon/t04_deepen01/T01_25083_print.java
@@ -0,0 +1,13 @@
+package baekjoon.t04_deepen01;
+
+// 새싹 출력
+public class T01_25083_print {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
